c965558886de6ad9b816240acb5bbe27f80dfb7f
keinn51 <soogood97@gmail.com>
2022-01-19 16:27:57 +0900

    Add regexpression for checking detail category
    Current Version : v1.1
    
    - what I did
    Original category name looks like "book>social".
    So I use regexpression to catch "social" in that string.
    /(.+?)>(.+)/ is the expression. I can use the expression by
    str.match(regexp).
    
    - What It is?
    Officially /(.+?)>(.+)/ Explain in the order I think.
    
    1. // : contains any expression you want.
    2. /.>./ : Since the corresponding character is in the form of "character > character",
      expression . stands for any character.
    3. /.+>.+/ : + means more than one character.
    4. /(.+)>(.+)/ : Grouping is done using parentheses. This returns the first and second groups as elements of the array.
    
    "book>social", "book>social", "book", "social"] uses the returned format.

--- JS_Emotion/front/src/components/CategoriesRecommend/index.jsx
+++ JS_Emotion/front/src/components/CategoriesRecommend/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Favorites from '@utils/db';
+import Category_List from '@utils/category_name';
 
 function CategoriesRecommend() {
   console.log(Favorites);
   return (
     <div>
       {Favorites.map((book) => (
-        <span>{book.categoryName}</span>
+        <span>{Category_List[book.categoryId].match(/(.+)>(.+)/)[2]}</span>
       ))}
     </div>
   );
