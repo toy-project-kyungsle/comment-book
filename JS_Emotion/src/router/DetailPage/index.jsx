e46f5841aad512f2a301cd05160e63bb19400eed
keinn51 <soogood97@gmail.com>
2022-02-10 10:44:12 +0900

    Add TextareaAutosize for comment part
    Current Version : v1.2
    
    - What I make
    I add the autosize package called "react-textarea-autosize".
    
    - The reason to do this
    I added that for sizing the comment part of DetailPage.
    
    - Principals of them
    Just add the package and import the module of TextareaAutosize.
    It is very powerfull and awesome!!

--- JS_Emotion/src/router/DetailPage/index.jsx
+++ JS_Emotion/src/router/DetailPage/index.jsx
@@ -5,15 +5,15 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import {
   Container,
-  Description,
   ImgDiv,
-  LeftSection,
   Letters,
   ShortView,
   Background,
   LetterGrid,
   AddEditComment,
+  OnelineTextArea,
 } from './styles';
+import TextareaAutosize from 'react-textarea-autosize';
 
 function DetailPage() {
   const { isbn } = useParams();
@@ -36,46 +36,46 @@ function DetailPage() {
       ) : (
         <Background>
           <Container>
-            <LeftSection>
-              <ShortView>
-                <ImgDiv>
-                  <img src={book.coverLargeUrl} alt={book.title} />
-                </ImgDiv>
-                <Letters>
-                  <h3>
-                    <a href={book.link}>{book.title}</a>
-                  </h3>
-                  <LetterGrid>
-                    {book.author ? (
-                      <>
-                        <div>author</div>
-                        <div>{book.author}</div>
-                      </>
-                    ) : null}
-                    <div>rating</div>
-                    <div>⭐️⭐️⭐️⭐️⭐️</div>
-                    {book.categoryId ? (
-                      <>
-                        <div>genre</div>
-                        <div>{GetDetailedName(book.categoryId)}</div>{' '}
-                      </>
-                    ) : null}
-                    {book.pubDate ? (
-                      <>
-                        <div>pubdate</div>
-                        <div>{book.pubDate}</div>
-                      </>
-                    ) : null}
-                  </LetterGrid>
-                </Letters>
-                <AddEditComment>
-                  <button>Add or Edit Comment</button>
-                </AddEditComment>
-              </ShortView>
-              <Description>
-                <p>{book.description}</p>
-              </Description>
-            </LeftSection>
+            <ShortView>
+              <ImgDiv>
+                <img src={book.coverLargeUrl} alt={book.title} />
+              </ImgDiv>
+              <Letters>
+                <h3>
+                  <a href={book.link}>{book.title}</a>
+                </h3>
+                <LetterGrid>
+                  {book.author ? (
+                    <>
+                      <div>작가</div>
+                      <div>{book.author}</div>
+                    </>
+                  ) : null}
+                  <div>평점</div>
+                  <div>⭐️⭐️⭐️⭐️⭐️</div>
+                  {book.categoryId ? (
+                    <>
+                      <div>장르</div>
+                      <div>{GetDetailedName(book.categoryId)}</div>{' '}
+                    </>
+                  ) : null}
+                  {book.pubDate ? (
+                    <>
+                      <div>출판일</div>
+                      <div>{book.pubDate}</div>
+                    </>
+                  ) : null}
+                  <div>한줄 평</div>
+                  <div>
+                    <OnelineTextArea></OnelineTextArea>
+                  </div>
+                </LetterGrid>
+              </Letters>
+            </ShortView>
+            <TextareaAutosize style={{ width: '100%' }} minRows={10} />
+            <AddEditComment>
+              <button>Add or Edit Comment</button>
+            </AddEditComment>
           </Container>
         </Background>
       )}
--- JS_Emotion/src/router/DetailPage/styles.js
+++ JS_Emotion/src/router/DetailPage/styles.js
@@ -1,42 +1,31 @@
 import styled from '@emotion/styled';
 
-const background_color = 'white';
 const container_color = '#ececec';
 
 export const Background = styled.div`
-  background-color: ${background_color};
-  height: 90%;
+  min-height: 90%;
+  width: 70%;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  margin: 0 auto;
 `;
 
 export const Container = styled.div`
+  position: relative;
   background-color: ${container_color};
 
-  display: flex;
-  flex-direction: row;
-
-  height: 500px;
-  width: 70%;
-  margin: 0 auto;
+  min-height: 500px;
+  width: 100%;
   padding: 20px;
 
   border-radius: 20px;
   box-shadow: 0px 19px 38px rgba(0, 0, 0, 0.3), 0px 15px 12px rgba(0, 0, 0, 0.22);
 `;
 
-export const LeftSection = styled.div`
-  z-index: 2;
-
-  height: 100%;
-  width: 100%;
-  /* border-right: 10px double #a7aabc; */
-  overflow: hidden;
-`;
-
 export const ShortView = styled.div`
-  position: relative;
   display: flex;
   flex-direction: row;
 `;
@@ -47,7 +36,7 @@ export const ImgDiv = styled.div`
 `;
 
 export const Letters = styled.div`
-  width: 65%;
+  width: 100%;
   margin-left: 50px;
   & a {
     text-decoration: none;
@@ -56,27 +45,20 @@ export const Letters = styled.div`
     text-shadow: 3px 3px #f2f3f3;
     margin: 30px 0 30px 0;
   }
-  & p {
-    margin: 30px 0 30px 0;
-  }
+`;
+
+export const OnelineTextArea = styled.textarea`
+  width: 100%;
 `;
 
 export const AddEditComment = styled.div`
   position: absolute;
-  right: 0;
-  top: 0;
+  right: 20px;
 `;
 
 export const LetterGrid = styled.div`
+  height: 225px;
   display: grid;
   grid-template-columns: 100px 2fr;
-  grid-auto-rows: minmax(40px, auto);
-`;
-
-export const Description = styled.div`
-  width: 100%;
-  height: 100%;
-  margin-top: 30px;
-  padding-right: 10px;
-  text-align: justify;
+  grid-auto-rows: minmax(30px, auto);
 `;
