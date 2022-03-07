e0ebcd96934f8f4d6d9641deec09a300864c1090
keinn51 <soogood97@gmail.com>
2022-03-07 16:07:23 +0900

    Fix #10 paste the index.html to 404.html
    Current Version : v2.0
    
    - What problem
    After deploying to github pages, there was a problem in which a 404
    page was displayed when refreshed on a routed page.
    
    - The reason to the error
    github pages do not support SPA. So, even if the URL is refreshed at https://[domain],
    the page looks good, but when reloading at https://[domain]/main, the GitHub page server
    does not know /main, so a 404 error and 404.html page are returned.
    
    - How to fix
    When using GitHub Pages, it's a shortcut, but I use 404.html a lot.
    It's because it provides it by itself. In CI, you just need to add a script
    that copies index.html to 404.html ;)

--- TS_Sass/src/components/MyInfo/index.tsx
+++ TS_Sass/src/components/MyInfo/index.tsx
@@ -21,20 +21,24 @@ function MyInfo({ loading, setLoadNum, setShowLoginModal }: Props) {
   const [infoLoading, setInfoLoading] = useState(true);
 
   const getMyInfo = useCallback(async () => {
-    let dataArr = Object.entries((await getDoc(doc(dbService, `UserEval`, authService.currentUser.uid))).data());
-    dataArr.sort((a, b) => b[1]['rating'] - a[1]['rating']);
-    let CategoryObj: any = {};
-    dataArr?.forEach((elem) => {
-      if (GetDetailedName(elem[1]['categoryId']) !== '') {
-        CategoryObj[elem[1]['categoryId']]
-          ? (CategoryObj[elem[1]['categoryId']] += 1)
-          : (CategoryObj[elem[1]['categoryId']] = 1);
-      }
-    });
-    setBestBook(dataArr[0][1]['title']);
-    setBookCount(dataArr.length);
-    setBestCategory(GetDetailedName(Number(Object.entries(CategoryObj).sort((a: any, b: any) => b[1] - a[1])[0][0])));
-    setInfoLoading(false);
+    const dbBook = (await getDoc(doc(dbService, `UserEval`, authService.currentUser.uid))).data();
+
+    if (dbBook) {
+      const dataArr = Object.entries(dbBook);
+      dataArr.sort((a, b) => b[1]['rating'] - a[1]['rating']);
+      const CategoryObj: any = {};
+      dataArr?.forEach((elem) => {
+        if (GetDetailedName(elem[1]['categoryId']) !== '') {
+          CategoryObj[elem[1]['categoryId']]
+            ? (CategoryObj[elem[1]['categoryId']] += 1)
+            : (CategoryObj[elem[1]['categoryId']] = 1);
+        }
+      });
+      setBestBook(dataArr[0][1]['title']);
+      setBookCount(dataArr.length);
+      setBestCategory(GetDetailedName(Number(Object.entries(CategoryObj).sort((a: any, b: any) => b[1] - a[1])[0][0])));
+      setInfoLoading(false);
+    }
     if (!infoLoading) setLoadNum((prev) => prev + 1);
   }, [infoLoading, setLoadNum]);
 
