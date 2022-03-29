c2dc2af24d8a370e43944a9cec41c3272cf23698
keinn51 <soogood97@gmail.com>
2022-03-29 16:01:05 +0900

    Refactor the useSliderBtn using EventLister
    Current Version : v2.2
    
    With EventListner, I can get window page width dynamically.

--- src/hooks/notReUsable/useSlideBtn.ts
+++ src/hooks/notReUsable/useSlideBtn.ts
@@ -6,29 +6,40 @@ const useSlideBtn = (
   bookCount: number,
 ): [number, React.Dispatch<React.SetStateAction<number>>, () => void, () => void] => {
   const [trans, setTrans] = useState(initialData);
+  const [transWidth, setTransWidth] = useState(0);
+  const [transLeftLimit, setTransLeftLimit] = useState(0);
 
-  let transWidth: number;
-  let transLeftLimit: number;
+  const getTransValue = useCallback(() => {
+    if (window.matchMedia('(max-width: 1000px) and (min-width: 735px)').matches) {
+      setTransWidth(ImgWidth * 3 + ImgLeftRighMargin * (2 * 2));
+      setTransLeftLimit(-(((bookCount - 3) / 3) * (ImgWidth * 3 + ImgLeftRighMargin * (2 * 2))));
+    } else if (window.matchMedia('(max-width: 735px) and (min-width: 470px)').matches) {
+      setTransWidth(ImgWidth * 2 + ImgLeftRighMargin * (2 * 1));
+      setTransLeftLimit(-(((bookCount - 2) / 2) * (ImgWidth * 2 + ImgLeftRighMargin * (2 * 1))));
+    } else if (window.matchMedia('(max-width: 470px)').matches) {
+      setTransWidth(ImgWidth);
+      setTransLeftLimit(-(bookCount * ImgWidth));
+    } else {
+      setTransWidth(ImgWidth * 4 + ImgLeftRighMargin * (2 * 3));
+      setTransLeftLimit(-(((bookCount - 4) / 4) * (ImgWidth * 4 + ImgLeftRighMargin * 6)));
+    }
+  }, [bookCount]);
 
-  if (window.matchMedia('(max-width: 1000px) and (min-width: 735px)').matches) {
-    transWidth = ImgWidth * 3 + ImgLeftRighMargin * (2 * 2);
-    transLeftLimit = -(((bookCount - 3) / 3) * (ImgWidth * 3 + ImgLeftRighMargin * (2 * 2)));
-  } else if (window.matchMedia('(max-width: 735px) and (min-width: 470px)').matches) {
-    transWidth = ImgWidth * 2 + ImgLeftRighMargin * (2 * 1);
-    transLeftLimit = -(((bookCount - 2) / 2) * (ImgWidth * 2 + ImgLeftRighMargin * (2 * 1)));
-  } else if (window.matchMedia('(max-width: 470px)').matches) {
-    transWidth = ImgWidth;
-    transLeftLimit = -(bookCount * ImgWidth);
-  } else {
-    transWidth = ImgWidth * 4 + ImgLeftRighMargin * (2 * 3);
-    transLeftLimit = -(((bookCount - 4) / 4) * (ImgWidth * 4 + ImgLeftRighMargin * 6));
-  }
+  useEffect(() => {
+    getTransValue();
+    window.addEventListener('resize', getTransValue);
+    return () => {
+      window.removeEventListener('resize', getTransValue);
+    };
+  }, [getTransValue]);
 
   useEffect(() => {
     return () => {
       setTrans(0);
     };
   }, [transWidth]);
+
+  // console.log('rerender!');
 
   const onClickL = useCallback(() => {
     if (trans >= 0) {
